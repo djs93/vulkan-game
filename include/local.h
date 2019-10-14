c3c935caa944195f4ce18740609a61ebb172ae6b
Dale Schofield <djs93@njit.edu>
2019-10-14 10:07:21 -0400

    Forgot to add global variable file

---
+++ include/local.h
@@ -0,0 +1,21 @@
+#ifndef __LOCAL_H__
+#define __LOCAL_H__
+
+// edict->movetype values
+typedef enum
+{
+	MOVETYPE_NONE,			// never moves
+	MOVETYPE_NOCLIP,		// origin and angles change with no interaction
+	MOVETYPE_PUSH,			// no clip to world, push on box contact
+	MOVETYPE_STOP,			// no clip to world, stops on box contact
+
+	MOVETYPE_WALK,			// gravity
+	MOVETYPE_STEP,			// gravity, special edge handling
+	MOVETYPE_FLY,
+	MOVETYPE_TOSS,			// gravity
+	MOVETYPE_FLYMISSILE,	// extra size to monsters
+	MOVETYPE_BOUNCE
+} movetype;
+
+#endif
+#pragma once
