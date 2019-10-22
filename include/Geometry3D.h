6eb0d974a098ce323715391503d2880f0b03a438
Dale <djs93@njit.edu>
2019-10-22 12:52:04 -0400

    Started 3D shapes, taking a break

--- include/Geometry3D.h
+++ include/Geometry3D.h
@@ -33,4 +33,85 @@ Ray FromPoints(Point3D from, Point3D to);
 
 void NormalizeDirection(Ray ray);
 
+typedef struct Sphere {
+	Point3D position;
+	float radius;
+} Sphere;
+
+/**
+ * @brief create and return a Sphere
+ * @param position the starting position of the center of the sphere
+ * @param radius the radius of the sphere
+ */
+Sphere sphere(Point3D position, float radius);
+
+typedef struct AABB {
+	Point3D position;
+	Vector3D size;
+} AABB;
+
+/**
+ * @brief create and return an Axis-Aligned Bounding Box (AABB)
+ * @param origin the starting position of the center of the box
+ * @param size the size of the box (half-extents)
+ */
+AABB aabb(Point3D origin, Vector3D size);
+
+Vector3D GetMin(AABB aabb);
+Vector3D GetMax(AABB aabb);
+AABB FromMinMax(Vector3D min, Vector3D max);
+
+typedef struct OBB {
+	Point3D position;
+	Vector3D size;
+	mat3 orientation;
+} OBB;
+
+/**
+ * @brief create and return an Oriented Bounding Box (OBB)
+ * @param origin the starting position of the center of the box
+ * @param size the size of the box (half-extents)
+ * @param orientation the rotation matrix of the OBB
+ */
+OBB obb(Point3D origin, Vector3D size, mat3 orientation);
+
+typedef struct Plane {
+	Vector3D normal;
+	float distance;
+} Plane;
+
+/**
+ * @brief create and return a plane
+ * @param normal the normal vector to the plane
+ * @param distance the distance to the plane from the origin
+ */
+Plane plane(Vector3D normal, float distance);
+
+/**
+* @brief used to detect if a point is on a plane
+* @param point the point to test
+* @param plane the place to test against
+* @return 0 if point is on plane, positive if in front, negative if behind
+*/
+float PlaneEquation(Point3D point, Plane plane);
+
+typedef struct Triangle {
+	union {
+		struct {
+			Point3D a;
+			Point3D b;
+			Point3D c;
+		};
+		Point3D points[3];
+		float values[9];
+	};
+} Triangle;
+
+/**
+* @brief create and return a triangle
+* @param p1 the first point of the triangle
+* @param p2 the second point of the triangle
+* @param p3 the third point of the triangle
+*/
+Triangle triangle(Point3D p1, Point3D p2, Point3D p3);
 #endif
--- src/Geometry3D.c
+++ src/Geometry3D.c
@@ -35,3 +35,85 @@ void NormalizeDirection(Ray ray)
 {
 	vector3d_normalize(&ray.direction);
 }
+
+Sphere sphere(Point3D position, float radius)
+{
+	Sphere s;
+	s.position = position;
+	s.radius = radius;
+	return s;
+}
+
+AABB aabb(Point3D origin, Vector3D size)
+{
+	AABB a;
+	a.position = origin;
+	a.size = size;
+	return a;
+}
+
+Vector3D GetMin(AABB aabb)
+{
+	Vector3D p1;
+	vector3d_add(p1, aabb.position, aabb.size);
+	Vector3D p2;
+	vector3d_sub(p2,aabb.position,aabb.size);
+
+	return vector3d(fminf(p1.x, p2.x),
+		fminf(p1.y, p2.y),
+		fminf(p1.z, p2.z));
+}
+
+Vector3D GetMax(AABB aabb)
+{
+	Vector3D p1;
+	vector3d_add(p1, aabb.position, aabb.size);
+	Vector3D p2;
+	vector3d_sub(p2,aabb.position,aabb.size);
+
+	return vector3d(fmaxf(p1.x, p2.x),
+		fmaxf(p1.y, p2.y),
+		fmaxf(p1.z, p2.z));
+}
+
+AABB FromMinMax(Vector3D min, Vector3D max)
+{
+	Vector3D add;
+	Vector3D sub;
+	vector3d_add(add, max, min);
+	vector3d_sub(sub, max, min);
+	vector3d_scale(add, add, 0.5f);
+	vector3d_scale(sub, sub, 0.5f);
+	return aabb(add, sub);
+}
+
+OBB obb(Point3D origin, Vector3D size, mat3 orientation)
+{
+	OBB box;
+	box.position = origin;
+	box.size = size;
+	box.orientation = orientation;
+	return box;
+}
+
+Plane plane(Vector3D normal, float distance)
+{
+	Plane p;
+	p.normal = normal;
+	p.distance = distance;
+	return p;
+}
+
+float PlaneEquation(Point3D point, Plane plane)
+{
+	return vector3d_dot_product(point, plane.normal) - plane.distance;
+}
+
+Triangle triangle(Point3D p1, Point3D p2, Point3D p3)
+{
+	Triangle tri;
+	tri.a = p1;
+	tri.b = p2;
+	tri.c = p3;
+	return tri;
+}
